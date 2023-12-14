22f04e1138391648dd854a2bca8134103d71df6b
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-12-15 02:13:48 +0330

    create file dataMuneItem done

---
+++ public/Data/MuneData.jsx
@@ -0,0 +1,14 @@
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+import WorkIcon from '@mui/icons-material/Work';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+export const dataMuneItem = [
+  { id: 1,title:'صفحه اصلی', icon: <HomeIcon/>, href: "/" },
+  { id: 2,title:'درباره من', icon: <PersonIcon/>, href: "/about-us" },
+  { id: 3,title:'رزومه', icon: <ContactPageIcon/>, href: "/resume" },
+  { id: 4,title:'نمونه کارها', icon: <WorkIcon/>, href: "/portfolio" },
+  { id: 5,title:'بلاگ', icon: <NewspaperIcon/>, href: "/blog" },
+  { id: 6,title:'تماس بامن', icon: <MailOutlineIcon/>, href: "/contact" },
+];
